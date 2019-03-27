d822d4d330ea751cdc10fc719e57e066b8464eed
DaanBorst <48981762+DaanBorst@users.noreply.github.com>
2019-03-27 09:21:22 +0100

    Update Login.php

--- Login.php
+++ Login.php
@@ -1,14 +1,54 @@
-<?php
-$servername = "localhost";
-$username = "username";
-$password = "password";
+<?php 
+//als formulier verzonden 
+if (!empty($_POST['username']) && !empty($_POST['password'])) { 
+    //controleer wachtwoord 
+     
+    //nog in te vullen 
+     
+    //wachtwoord correct 
+    if ($nog_in_te_vullen) { 
+        $login_correct = TRUE; 
+    } 
+    //wachtwoord niet correct 
+    else { 
+        $login_error = TRUE; 
+    } 
+} 
+?> 
 
-//Create connection
-$conn = new mysqli($servername,$username,$password);
+<!DOCTYPE HTML> 
+<html> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+<title>Login</title> 
+</head> 
+<body> 
 
-//Check connection nibba
-if ($conn->connect_error) {
-die("Connection failed:".$conn->connect_error);
-}
-echo"Connected Successfully";
-?>
+<?php if ($login_correct === TRUE) { ?> 
+     
+    <h1>Login gelukt!</h1> 
+    <p>Welkom in het beveiligde gedeelte van deze website!</p> 
+     
+<?php } else { ?> 
+     
+    <h1>Login</h1> 
+    <p>Vul gebruikersnaam en wachtwoord in om toegang te krijgen tot het beveiligde gedeelte van deze website</p> 
+     
+    <?php 
+    if ($login_error === TRUE) { 
+        echo '<p class="error">De gebruikersnaam/wachtwoord combinatie bestaat niet.</p>'; 
+    } 
+    ?> 
+     
+    <form method="post"> 
+    <table> 
+    <tr><td>Gebruikersnaam:</td><td><input type="text" name="username"></td></tr> 
+    <tr><td>Wachtwoord:</td><td><input type="password" name="password"></td></tr> 
+    <tr><td></td><td><input type="submit" value="Login"></td></tr> 
+    </table> 
+    </form> 
+     
+<?php } ?> 
+
+</body> 
+</html>
